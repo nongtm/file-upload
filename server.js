e8a9a6face5c73dad216a7142b0ec839daf0cc68
Tri Nong <minhtri1296@gmail.com>
2016-08-30 22:44:18 +0000

    Final Testing

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
 var express = require('express');
 var app = express();
 
-//lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
-//We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
+var multer  = require('multer')
+var upload = multer({ dest: './uploads/' })
 
-// Connection URL. This is where your mongodb server is running.
-
-var url = process.env.MONGOLAB_URI;      
-
-// Use connect method to connect to the Server
-  MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
-
-    // do some work here with the database.
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 });
+
+app.post('/', upload.single('up-file'), function (req, res) {
+  if (req.file) {
+    res.send('The size of your file is: ' + req.file.size +'bytes');
+  }
+  else
+  res.send('Missing file!');
+})
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
-    //Close connection
-    db.close();
-  }
-});
+
 
